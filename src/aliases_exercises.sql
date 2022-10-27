86a5ac0036ef44b936524d80e9853df6040ae2d5
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-27 14:37:57 -0500

    Updates to database exrcises

--- src/aliases_exercises.sql
+++ src/aliases_exercises.sql
@@ -1,3 +1,9 @@
-USE employees;
+# USE employees;
+#
+# SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB FROM employees LIMIT 10;
+#
+# DESCRIBE employees;
 
-SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB FROM employees LIMIT 10;
+# USE codeup_test_db;
+
+ALTER TABLE albums ADD UNIQUE (name, artist);
