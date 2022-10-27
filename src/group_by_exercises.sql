b04153d0a46f99ad8c3daf0d2662a723cc542fc7
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-27 11:14:16 -0500

    Updates to aliases exercises

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -1,12 +1,14 @@
 USE employees;
 
-SELECT DISTINCT title, COUNT(title) FROM titles GROUP BY title;
+SELECT DISTINCT title FROM titles;
 
 SELECT DISTINCT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' GROUP BY last_name;
 
-SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' GROUP BY last_name, first_name;
 
-SELECT DISTINCT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+SELECT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
-SELECT DISTINCT gender, COUNT(gender) FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+SELECT COUNT(last_name) AS count, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT COUNT(gender) AS gender_count, gender FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' GROUP BY gender;
 
